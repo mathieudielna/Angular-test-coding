f150e13705438ee6197f4a864836f5e4fcb740c2
Mathieu DIELNA <DIM@lusis.domain>
2022-09-22 14:33:24 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from  '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ReactiveForm';
+export class AppComponent implements OnInit{
+  public form: FormGroup = new FormGroup(
+      {nom: new FormControl(''),
+       email: new FormControl(''),
+       password: new FormControl(''),
+       confirmation: new FormControl(''),
+    });
+
+  constructor(){}
+
+  ngOnInit(): void {}
+
+  //Default : setValue({Tous les champs}) - pacthValue({ un ou plusieurs champs})
+  //Control : removeControl('nom') - addControl({'nom', new formControl('')) - setControl('', new FormControl(''))
+  //GetControl : get('nom')
+  //reset : this.form.reset('nom')
+
+  public submit() {
+    console.log(this.form.getRawValue())
+    console.log(this.form.value);
+  }
 }
