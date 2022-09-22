468b446d351d4f1cb128020571cf282806b9a159
Mathieu Dielna <mathieudielna.pro@gmail.com>
2022-09-22 21:58:21 +0200

    end experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,49 @@ import { FormGroup, FormControl, Validators } from  '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  @ViewChild("confirmation") 
-  public el?: ElementRef<HTMLInputElement>;
+  // declarations
+  @ViewChild("confirmation", {static: true})
+  public el!: ElementRef<HTMLInputElement>;
   public form: FormGroup = new FormGroup(
       {nom: new FormControl('', [Validators.required, this.validatorsMat]),
-       email: new FormControl('', Validators.minLength(2)),
-       password: new FormControl('', this.passwordMatch(this.form)),
+       email: new FormControl('', [Validators.required ,Validators.minLength(2)]),
+       password: new FormControl('', [Validators.required]),
     });
 
+  constructor(){}
+
+    // get form fields
     get password() {
-      return this.form.get("password"); 
+      return this.form.get("password");
     }
 
     get confirmedPassword() {
-      return this.form.get("confirmation"); 
+      return this.form.get("confirmation");
     }
 
+    // function validator
     validatorsMat(formControl : FormControl): {[x: string]: boolean} {
       if (formControl.value === 'mat') {
         return {notMat : true};
      }
-      return {notMat : false};    
+      return {notMat : false};
      }
 
      passwordMatch(formControl: FormControl): {[x:string] : boolean} {
-      if (formControl.value !== this.el?.nativeElement.value) {
+      if (formControl.value !== this.el!.nativeElement.value) {
+        // this.el = `<p> les champs : mots de passe  et confirmations doivent correspondre !</p>`;
         return { notMatching: true};
       }
+      // if ( ) {
+      //   return { notMatching: false};
+      // }
       return { notMatching: false};
-     }
-    
-  constructor(){}
 
+     }
+
+
+
+  //onInit
   ngOnInit(): void {}
 
   //Default : setValue({Tous les champs}) - pacthValue({ un ou plusieurs champs})
@@ -46,6 +57,8 @@ export class AppComponent implements OnInit{
   //GetControl : get('nom')
   //reset : this.form.reset('nom')
 
+
+  //formSubmit
   public submit() {
     console.log(this.form.getRawValue())
     console.log(this.form.value);
